624b072631abd57a1d5d96d43b67a92b76bc7046
Andrew Bates <abates@omeganetserv.com>
2019-09-20 07:55:09 -0400

    More examples

--- scraper.go
+++ scraper.go
@@ -38,4 +38,25 @@
 //			fmt.Printf("%+v\n", v)
 //			// &{Name:Hello Scraper! URL:https://github.org/mh-orange/scraper}
 //		}
+//
+// Structs are unmarshaled by matching CSS selectors to elements in an html document
+// tree.  Scraper uses the wonderful Cascadia (https://github.com/andybalholm/cascadia)
+// package to parse and match CSS selectors.
+//
+// To specify matching and unmarshaling rules, use the "scraper" and "scrapeType" struct
+// field tags.  The "scraper" tag is used to define the CSS selector and the "scrapeType"
+// indicates whether the value should be the text content or an attribute
+// of the matching element.  The default type (if the scrapeTag is omitted) is to use
+// the text content.  For example, to match an element with the id "name" and
+// capture its text content:
+//		type MyType struct {
+//			Name string `scraper:"#name"`
+//		}
+//
+// Another example, which uses the href attribute of a matching "a" tag:
+//		type MyType struct {
+//			URL string `scraper:"a" scrapeType:"attr:href"`
+//		}
+// Note that the attribute name is specified after the type (attr) and a separating colon.
+//
 package scraper
--- scraper_test.go
+++ scraper_test.go
@@ -2,6 +2,7 @@ package scraper_test
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/mh-orange/scraper"
 )
@@ -10,8 +11,11 @@ func ExampleUnmarshal() {
 	// Parse and unmarshal an HTML document into a very basic Go struct
 	document := `<html><body><h1 id="name">Hello Scraper!</h1><a href="https://github.org/mh-orange/scraper">Scraper</a> is Grrrrrreat!</body></html>`
 	v := &struct {
-		Name string `scraper:"#name"`                    // Match the element with the ID "name" and set Name to the text content
-		URL  string `scraper:"a" scrapeType:"attr:href"` // match the first A element and set URL to the value of the HREF attribute
+		// Name is assigned the text content from the element with the ID "name"
+		Name string `scraper:"#name"`
+
+		// URL is assigned the HREF attribute of the first A element found
+		URL string `scraper:"a" scrapeType:"attr:href"`
 	}{}
 	err := scraper.Unmarshal([]byte(document), v)
 	if err != nil {
@@ -22,8 +26,8 @@ func ExampleUnmarshal() {
 }
 
 func ExampleUnmarshal_slice() {
-	// Parse and unmarshal an HTML document into a Go struct
-	// with a slice of things
+	// Scraper can be used to unmarshal structs with slices
+	// of things as well
 	document := `
 		<html>
 			<body>
@@ -36,11 +40,11 @@ func ExampleUnmarshal_slice() {
 			</body>
 		</html>`
 	v := &struct {
-		// Match the element with the ID "name" and set Name to the text content
+		// Name is assigned the text content from the element with the ID "name"
 		Name string `scraper:"#name"`
 
-		// find all elements matching "ul li", for each of them, create a new entry in
-		// Items with the value set to the text content of the matching nodes
+		// Items is appended with the text content of each element matching the
+		// "ul li" CSS selector
 		Items []string `scraper:"ul li"`
 	}{}
 	err := scraper.Unmarshal([]byte(document), v)
@@ -49,4 +53,51 @@ func ExampleUnmarshal_slice() {
 	}
 	fmt.Printf("%+v\n", v)
 	// Output: &{Name:Hello Scraper! Items:[Item 1 Item 2 Item 3]}
+}
+
+func ExampleUnmarshal_nested() {
+	// Scraper can be used to unmarshal structs with other structs
+	// in them
+	document := `
+		<html>
+			<body>
+				<h1 id="name">Hello Scraper!</h1>
+				<ul>
+					<li>Item 1</li>
+					<li>Item 2</li>
+					<li>Item 3</li>
+				</ul>
+			</body>
+		</html>`
+	v := &struct {
+		// Name is assigned the text content from the element with the ID "name"
+		Name string `scraper:"#name"`
+
+		// Items is matched with the ul tag and then names is matched by the
+		// li tags within.  Nested structs will be unmarshaled with the matching
+		// _subtree_ not the entire document
+		Items struct {
+			Names []string `scraper:"li"`
+		} `scraper:"ul"`
+	}{}
+	err := scraper.Unmarshal([]byte(document), v)
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Printf("%+v\n", v)
+	// Output: &{Name:Hello Scraper! Items:{Names:[Item 1 Item 2 Item 3]}}
+}
+
+func ExampleDecoder() {
+	// Decoder is useful for unmarshaling from an input stream
+	document := `<html><body><h1 id="name">Hello Scraper!</h1></body></html>`
+	v := &struct {
+		// Name is assigned the text content from the element with the ID "name"
+		Name string `scraper:"#name"`
+	}{}
+
+	reader := strings.NewReader(document)
+	scraper.NewDecoder(reader).Decode(v)
+	fmt.Printf("%+v\n", v)
+	// Output: &{Name:Hello Scraper!}
 }
